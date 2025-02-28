8013434ad98c5b989bb46f0dd740a812f7cba0a0
Rishi <rishibhateja700@gmail.com>
2025-02-28 14:41:26 +0530

    Initial commit

---
+++ fullstack/src/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Cristiano </h1>
+            <p>Contact: Cristiano@gmail.com</p>
+            <p>Phone: +919999988888</p>
+        </div>
+            );
+};
+
+export default Home;
